44e4cae2396cee1e8a8399796e56ab085e852d65
Hao-Jui <94800509+Hao-Jui@users.noreply.github.com>
2026-03-20 19:24:45 -0500

    Corrected brent.f90

--- src/core/starting_model.f90
+++ src/core/starting_model.f90
@@ -51,7 +51,7 @@ contains
   contains
     subroutine single_model()
       real(wp) :: ee, rho0, t0, t1
-      !r_ratio = .7e0_wp
+      r_ratio = .7e0_wp
       output = .true.; call cpu_time(t0)
       call rotation_solver
       call solution_properties
