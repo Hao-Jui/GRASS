fd59fbcd0b3c45b764c96212e8a574df7f1d5b8f
Hao-Jui <94800509+Hao-Jui@users.noreply.github.com>
2026-08-10 00:43:43 -0500

    chore: update EOS comparison script

--- eos/check_eos.m
+++ eos/check_eos.m
@@ -1,14 +1,25 @@
 clear
 
-dir = "/Users/horay/CodesArchiv/rns_universal/eos/muses.dat";
-A = importdata(dir," ",1);
 
-e = A.data(:,1);
-p = A.data(:,2);
-h = log( A.data(:,3) );
-n = A.data(:,4);
+EOS = ["DD2_hot_equal_pinned" "MPA1"];
 
-plot(p,e);
+n_eos = numel(EOS);
 
-set(gca,'xscale','log')
-set(gca,'yscale','log')
+figure(3); hold on; box on;
+for i=1:n_eos
+    dir = strcat("/Users/horay/ptmp/GRASS/eos/",EOS(i),".dat");
+    A = importdata(dir," ",1);
+    e = A.data(:,1);
+    p = A.data(:,2);
+    h = A.data(:,3);
+    n = A.data(:,4);
+    plot( e, p, '-o' )
+end
+
+set(gca,'xscale','log'); set(gca,'yscale','log');
+legend('Interpreter', 'latex', 'FontSize', 20,'Location','northwest')
+
+xlabel('$e$', 'Interpreter', 'latex');
+ylabel('$p$', 'Interpreter', 'latex');
+set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', 25, ...
+    'LabelFontSizeMultiplier', 1.1, 'TickDir', 'in');
